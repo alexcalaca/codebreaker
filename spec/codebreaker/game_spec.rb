1d4200d4986896d61cf94a1b73455251c4b66f03
AC Alexandre Calaça <alexcalaca@gmail.com>
2023-03-24 11:11:19 -0300

    test: add test prompts for the first guess

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -13,6 +13,14 @@ module Codebreaker
         game.start
       end
 
+			it "prompts for the first guess" do
+        output = double('output').as_null_object
+        game = Game.new(output)
+
+        output.should_receive(:puts).with('Enter guess:')
+
+        game.start
+      end
     end    
   end
 end
