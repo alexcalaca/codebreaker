ac75c39fa6dd2941ac1301f2af2191a27d900bac
AC Alexandre Calaça <alexcalaca@gmail.com>
2023-03-24 11:10:40 -0300

    test: add test - sends a welcome message

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -2,6 +2,17 @@ require 'spec_helper'
 
 module Codebreaker
   describe Game do
+    describe "#start" do
+			it "sends a welcome message" do
+        #Given
+        output = double('output').as_null_object
+        game = Game.new(output)
 
+        output.should_receive(:puts).with('Welcome to Codebreaker!')
+
+        game.start
+      end
+
+    end    
   end
 end
