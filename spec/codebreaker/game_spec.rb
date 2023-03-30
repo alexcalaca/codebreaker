d0878bc0350c9ace2049168d3eedb4f496712b7e
AC Alexandre Calaça <alexcalaca@gmail.com>
2023-03-30 14:39:28 -0300

    test: create tests for #guess

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -15,6 +15,49 @@ module Codebreaker
         output.should_receive(:puts).with('Enter guess:')
         game.start('1234')
       end
-    end    
+    end
+
+    describe "#guess" do
+      context "with no matches" do
+        it "sends a mark with ''" do
+          game.start('1234')
+          output.should_receive(:puts).with('')
+          game.guess('5555')
+        end
+      end
+
+      context "with 1 number match" do
+        it "sends a mark with '-'" do
+          game.start('1234')
+          output.should_receive(:puts).with('-')
+          game.guess('2555')
+        end
+      end
+
+      context "with 1 exact match" do
+        it "sends a mark with '+'" do
+          game.start('1234')
+          output.should_receive(:puts).with('+')
+          game.guess('1555')
+        end
+      end
+
+      context "with 2 numbers matches" do
+        it "sends a mark with '--'" do
+          game.start('1234')
+          output.should_receive(:puts).with('--')
+          game.guess('2355')
+        end
+      end
+
+      context "with 1 number match and 1 exact match (in that order)" do
+        it "sends a mark with '+-'" do
+          game.start('1234')
+          output.should_receive(:puts).with('+-')
+          game.guess('2535')
+        end
+      end
+
+    end
   end
 end
